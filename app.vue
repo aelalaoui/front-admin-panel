b0c3b1f96ace7377f2252e7d106da67da0ebb16e
averoos <adil.el.alaoui.89@gmail.com>
2023-11-22 22:24:10 +0100

    add cards

--- app.vue
+++ app.vue
@@ -7,15 +7,78 @@
       <!-- Include the SideBar component -->
       <side-bar :show="sideBarShow" />
 
-      <v-main>
-        <v-container fluid>
-          <v-row dense>
-            <v-col v-for="n in 8" :key="n" cols="3">
-              <v-sheet color="surface-variant-alt" height="96"></v-sheet>
+      <v-container>
+        <template>
+          <v-row align="center" justify="center">
+            <v-col cols="auto">
+              <v-card
+                  class="mx-auto"
+                  max-width="344"
+                  title="Icons"
+                  subtitle="prepend-icon and append-icon"
+                  prepend-icon="mdi-account"
+                  append-icon="mdi-check"
+              >
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="auto">
+              <v-card
+                  class="mx-auto"
+                  max-width="344"
+                  title="Icons"
+                  subtitle="prepend and append"
+              >
+                <template v-slot:prepend>
+                  <v-icon icon="mdi-account" color="primary"></v-icon>
+                </template>
+                <template v-slot:append>
+                  <v-icon icon="mdi-check" color="success"></v-icon>
+                </template>
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="auto">
+              <v-card
+                  class="mx-auto"
+                  max-width="344"
+                  title="Avatars"
+                  subtitle="prepend-avatar and append-avatar"
+                  prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
+                  append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+              >
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+              </v-card>
+            </v-col>
+
+            <v-col cols="auto">
+              <v-card
+                  class="mx-auto"
+                  max-width="344"
+                  title="Avatars"
+                  subtitle="prepend and append"
+              >
+                <template v-slot:prepend>
+                  <v-avatar color="blue-darken-2">
+                    <v-icon icon="mdi-alarm"></v-icon>
+                  </v-avatar>
+                </template>
+                <template v-slot:append>
+                  <v-avatar size="24">
+                    <v-img
+                        src="https://cdn.vuetifyjs.com/images/john.png"
+                        alt="John"
+                    ></v-img>
+                  </v-avatar>
+                </template>
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+              </v-card>
             </v-col>
           </v-row>
-        </v-container>
-      </v-main>
+        </template>
+      </v-container>
     </v-layout>
   </v-card>
 </template>
--- component/sideBar.vue
+++ component/sideBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-navigation-drawer location="left" v-if="show">
-    <v-list>
+  <v-navigation-drawer location="left" v-if="show" theme="dark" class="bg-deep-purple">
+    <v-list color="transparent">
       <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
       <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
     </v-list>
