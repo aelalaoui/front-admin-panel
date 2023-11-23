5976ce85b2257fcadc9e73be5eee7b8166d03c65
averoos <adil.el.alaoui.89@gmail.com>
2023-11-23 22:12:08 +0100

    add papges and restructure nav and side bars

--- app.vue
+++ app.vue
@@ -1,99 +1,42 @@
 <template>
   <v-card class="mx-auto">
     <v-layout>
-      <!-- Include the NavBar component -->
-      <nav-bar @toggle-drawer="updateDrawerState" />
+      <!-- Include the NavBar components -->
+      <nav-bar @toggle-drawer="toggleSidebar" />
 
-      <!-- Include the SideBar component -->
-      <side-bar :show="sideBarShow" />
+      <!-- Include the SideBar components -->
+      <side-bar :show="sideBarShow" ref="sidebar"/>
 
-      <v-container>
-        <template>
-          <v-row align="center" justify="center">
-            <v-col cols="auto">
-              <v-card
-                  class="mx-auto"
-                  max-width="344"
-                  title="Icons"
-                  subtitle="prepend-icon and append-icon"
-                  prepend-icon="mdi-account"
-                  append-icon="mdi-check"
-              >
-                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col cols="auto">
-              <v-card
-                  class="mx-auto"
-                  max-width="344"
-                  title="Icons"
-                  subtitle="prepend and append"
-              >
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-account" color="primary"></v-icon>
-                </template>
-                <template v-slot:append>
-                  <v-icon icon="mdi-check" color="success"></v-icon>
-                </template>
-                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col cols="auto">
-              <v-card
-                  class="mx-auto"
-                  max-width="344"
-                  title="Avatars"
-                  subtitle="prepend-avatar and append-avatar"
-                  prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
-                  append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-              >
-                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
-              </v-card>
-            </v-col>
-
-            <v-col cols="auto">
-              <v-card
-                  class="mx-auto"
-                  max-width="344"
-                  title="Avatars"
-                  subtitle="prepend and append"
-              >
-                <template v-slot:prepend>
-                  <v-avatar color="blue-darken-2">
-                    <v-icon icon="mdi-alarm"></v-icon>
-                  </v-avatar>
-                </template>
-                <template v-slot:append>
-                  <v-avatar size="24">
-                    <v-img
-                        src="https://cdn.vuetifyjs.com/images/john.png"
-                        alt="John"
-                    ></v-img>
-                  </v-avatar>
-                </template>
-                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </template>
-      </v-container>
+      <!-- Markup shared across all pages -->
+      <NuxtPage />
     </v-layout>
   </v-card>
 </template>
 <script>
-import NavBar from "./component/navBar.vue";
-import SideBar from "./component/sideBar.vue";
+import NavBar from "./components/navBar.vue";
+import SideBar from "./components/sideBar.vue";
+
 export default {
   components: { NavBar, SideBar },
   data: () => ({
     sideBarShow: false,
   }),
-  methods: {
-    updateDrawerState(drawerState) {
-      this.sideBarShow = drawerState;
-    },
+  mounted() {
+    document.body.addEventListener("mousedown", this.closeSidebarOnClickOutside);
   },
-}
+  methods: {
+    toggleSidebar() {
+      this.sideBarShow = !this.sideBarShow;
+    },
+    closeSidebarOnClickOutside(event) {
+      const sidebarElement = this.$refs.sidebar && this.$refs.sidebar.$el;
+      if (sidebarElement && !event.target.closest('.sidebar-container')) {
+        this.sideBarShow = false;
+      }
+    }
+  },
+  beforeDestroy() {
+    document.body.removeEventListener("mousedown", this.closeSidebarOnClickOutside);
+  },
+};
 </script>
--- components/sideBar.vue
+++ components/sideBar.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-navigation-drawer location="left" v-if="show" theme="dark" class="bg-deep-purple">
+  <v-navigation-drawer
+      location="left"
+      theme="dark"
+      class="sidebar-container bg-deep-purple"
+      :class="{ 'transformed': show }"
+      :style="{ transform: show ? 'translateX(0%)' : 'translateX(-110%)' }"
+  >
     <v-list color="transparent">
       <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
       <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
@@ -14,3 +20,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+  .transformed {
+    transform: translateX(0%);
+  }
+</style>
